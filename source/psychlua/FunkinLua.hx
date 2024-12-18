ac93bf6b5e0e35014abcf7d4c57ecbf6d40d5e10
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-12-17 22:13:31 -0500

    Update FunkinLua.hx

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1858,16 +1858,6 @@ class FunkinLua {
 	}
 }
 #end
-					
-class ModchartView extends Flx3DView
-{
-	public var wasAdded:Bool = false;
-	public function new(?x:Float = 0, ?y:Float = 0, ?w:Int = 0, ?y:Int = 0)
-	{
-		super(x, y, w, y);
-		antialiasing = ClientPrefs.globalAntialiasing;
-	}
-}
 
 class ModchartText extends FlxText
 {
