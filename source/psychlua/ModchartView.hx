47de880d7590fee4c9af191730c8936d136204dc
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-12-17 22:40:30 -0500

    Update ModchartView.hx

--- source/psychlua/ModchartView.hx
+++ source/psychlua/ModchartView.hx
@@ -8,6 +8,6 @@ class ModchartView extends Flx3DView
 	public function new(?x:Float = 0, ?y:Float = 0, ?w:Int = 0, ?y:Int = 0)
 	{
 		super(x, y, w, y);
-		antialiasing = ClientPrefs.globalAntialiasing;
+		antialiasing = ClientPrefs.data.antialiasing;
 	}
 }
